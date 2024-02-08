3e81ba50b1a2476d49a1be0a4fb2b42e244c841a
Mohit Pandey <mp7698921@gmail.com>
2024-02-08 18:26:10 +0530

    making Home page responsive

--- food_recipe/settings.py
+++ food_recipe/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR ,"static"]
 
 MEDIA_ROOT = BASE_DIR /"media"
 MEDIA_URL = '/media/'
